055ded9eef9302dfbbae0d7b5dbaa7f40588d393
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-02-06 20:23:30 +0530

    "new origin cors added"

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const app = require("./app");
 app.use(
   cors({
     origin: [
+      "https://www.chronocrafts.xyz",
       "https://chronocrafts.xyz",
       "http://localhost:5173",
       "http://localhost:3000",
